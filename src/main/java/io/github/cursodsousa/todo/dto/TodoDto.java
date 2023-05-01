901115b7fd4e664513a431ae07ced41fd45b7830
ferreira2006 <ferreira2006@gmail.com>
2023-05-01 08:16:47 -0300

    carga inicial

---
+++ src/main/java/io/github/cursodsousa/todo/dto/TodoDto.java
@@ -0,0 +1,55 @@
+package io.github.cursodsousa.todo.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class TodoDto {
+	@NotBlank
+	@Size(min = 4)
+	private String description;
+
+	private Boolean done;
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime createdDate;
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime doneDate;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getDoneDate() {
+		return doneDate;
+	}
+
+	public void setDoneDate(LocalDateTime doneDate) {
+		this.doneDate = doneDate;
+	}
+
+}
---
+++ src/main/java/io/github/cursodsousa/todo/model/Todo.java
@@ -0,0 +1,82 @@
+package io.github.cursodsousa.todo.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+
+@Entity
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private Boolean done = Boolean.FALSE;
+	
+	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime createdDate;
+	
+	@Column
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime doneDate;
+
+	@PrePersist
+	public void beforeSave() {
+		setCreatedDate(LocalDateTime.now());
+	}
+	
+	public Todo() { }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getDoneDate() {
+		return doneDate;
+	}
+
+	public void setDoneDate(LocalDateTime doneDate) {
+		this.doneDate = doneDate;
+	}
+	
+}
